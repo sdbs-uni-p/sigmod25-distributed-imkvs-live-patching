9155ffd53e0876fe1ad7b9356b40ce45373b1821
Michael Fruth <Michael.Fruth@uni-passau.de>
2025-08-17 16:26:58 +0200

    Make data loading single-threaded

--- reproduction/redis-cluster/transformation/beder2/duckdb_storage.py
+++ reproduction/redis-cluster/transformation/beder2/duckdb_storage.py
@@ -105,124 +105,14 @@ class DuckDBStorageThread(DuckDBStorage, threading.Thread):
         super().close()
 
     def run(self):
-        # This is important! We have to finally fill all tables by the creation order because of references!
-        run_insert_lock = threading.Lock()
-
-        insert_pool = FutureCollector(ThreadPoolExecutor(max_workers=10))
-
-        staging_workers = {
-            tbl: self._StagingWorker(self._con.cursor(), tbl)
-            for tbl in self._get_tables_creation_order()
-        }
-        insert_con = self._con.cursor()
-
-        def append(table, df, wait_for_lock: bool = False):
-            # Abort
-            if df.empty:
-                self._queue.task_done()
-                return
-
-            if wait_for_lock:
-                # We just wait until the lock is free, but we don't need it ;-)
-                run_insert_lock.acquire()
-                run_insert_lock.release()
-            try:
-                print(f"[START] {datetime.datetime.now()} Insert {table}")
-                insert_con.append(table, df, by_name=True)
-                print(f"[END] {datetime.datetime.now()} Insert {table}")
-            except Exception as e:
-                print("ERROR")
-                print(e)
-                print(table)
-                print(df.columns)
-                print(df.dtypes)
-                print(df, flush=True)
-                raise e
-            self._queue.task_done()
-
         while True:
             table, df_input_data = self._queue.get()
 
             # Abort gracefully
             if table is None and df_input_data is None:
-                # Wait for pool to finish
-                insert_pool.shutdown()
-                insert_con.close()
-
-                for worker in staging_workers.values():
-                    worker.noblocking_close()
-                for worker in staging_workers.values():
-                    worker.wait()
-
                 self._queue.task_done()
                 break
-
-            # We insert run immediately!
-
-            if table in ["run", "latencies", "rdb"]:
-                # In order to insert run data, we have to lock the table!
-                run_insert_lock.acquire()
-                future = insert_pool.submit(append, table, df_input_data)
-                future.add_done_callback(lambda x: run_insert_lock.release())
-            # elif table in ["latencies", "rdb"]:
-            #     # If our run insert already has the lock, we wait until all run data is inserted!
-            #     insert_pool.submit(append, table, df_input_data, wait_for_lock=True)
-            else:
-                staging_workers[table].put(df_input_data)
-                self._queue.task_done()
-
-    class _StagingWorker(threading.Thread):
-        def __init__(self, con: duckdb.DuckDBPyConnection, table: str):
-            super().__init__()
-            self._con = con
-            # self._database_file = database_file
-            self._table: str = table
-            self._staging_table = f"{STAGING_PREFIX}{self._table}"
-            self._queue = mp.JoinableQueue()
-            self.start()
-
-        def run(self):
-            # con: duckdb.DuckDBPyConnection = duckdb.connect(database=self._database_file, read_only=False)
-
-            self._con.execute(
-                f"CREATE TEMP TABLE {self._staging_table} AS SELECT * FROM {self._table} WHERE 0 = 1"
-            )
-
-            while True:
-                df_input_data = self._queue.get()
-                if df_input_data is None:
-                    # Dump data
-                    print(f"[START] {datetime.datetime.now()} Flushing {self._table}")
-                    self._con.execute(
-                        f"INSERT INTO {self._table} SELECT * FROM {self._staging_table}"
-                    )
-                    print(f"[END] {datetime.datetime.now()} Flushing {self._table}")
-                    self._queue.task_done()
-                    break
-
-                print(f"[START] {datetime.datetime.now()} Append {self._staging_table}")
-                self._con.append(self._staging_table, df_input_data, by_name=True)
-                print(
-                    f"[END] {datetime.datetime.now()} Append {self._staging_table}",
-                    flush=True,
-                )
-
-                self._queue.task_done()
-
-            # We are done!
-            # super().close()
-
-        def put(self, val):
-            self._queue.put(val)
-
-        def close(self):
-            self._queue.join()
-            self._queue.put(None)
-            self._queue.join()
-            self._con.close()
-
-        def noblocking_close(self):
-            self._queue.put(None)
-
-        def wait(self):
-            self._queue.join()
+            print(f"[START] {datetime.datetime.now()} Insert {table}")
+            self._con.append(table, df_input_data, by_name=True)
+            print(f"[END] {datetime.datetime.now()} Insert {table}")
+            self._queue.task_done()
